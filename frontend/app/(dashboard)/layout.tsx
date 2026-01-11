9e9238d204564534130cb7769a65cc7805546e4b
Thanhngan1435 <phththngan2005@gmail.com>
2026-01-11 19:59:56 +0700

    thngan

--- frontend/app/(dashboard)/layout.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { ReactNode } from "react";
-import AppShellProtected from "@/components/layouts/AppShellProtected";
-
-export default function DashboardLayout({ children }: { children: ReactNode }) {
-  return <AppShellProtected>{children}</AppShellProtected>;
-}
-
