51ed9588a786804a003179f81cf1d2a9e47055cc
TrungTai-gif <taint1272@ut.edu.vn>
2026-01-19 21:09:38 +0700

    Thêm UsersInternalController vào module

--- services/user-service/src/users/users.module.ts
+++ services/user-service/src/users/users.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UsersInternalController } from './users-internal.controller';
 import { User, UserSchema } from './schemas/user.schema';
 import { AuthModule } from '@shared/common/auth/auth.module';
 
@@ -10,7 +11,7 @@ import { AuthModule } from '@shared/common/auth/auth.module';
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     AuthModule,
   ],
-  controllers: [UsersController],
+  controllers: [UsersController, UsersInternalController],
   providers: [UsersService],
   exports: [UsersService],
 })
